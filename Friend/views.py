e2fb7ec7723e24623fbe3001bcb4ee89bf7210bf
Shawn-NG <nghengdi10@gmail.com>
2021-11-08 17:03:43 +0000

    configure the POST method

--- Friend/views.py
+++ Friend/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from Friend.models import Friend
 from Friend.serializer import FriendSerializer
@@ -11,4 +12,17 @@ class FriendViewSet(viewsets.ModelViewSet):
     serializer_class = FriendSerializer
 
     def create(self, request):
-        print(request.data)
+        # {'user_friend_id_one': 'cf48c865-2db7-4be4-9006-d5595fca829d', 'user_friend_id_two': '35367163-c482-401d-b621-4c90f62594f6'}
+        friend_data_one = FriendSerializer(data=request.data)
+        friend_data_two = FriendSerializer(data={
+            'user_friend_id_one': request.data['user_friend_id_two'],
+            'user_friend_id_two': request.data['user_friend_id_one']
+        })
+
+        if friend_data_one.is_valid() and friend_data_two.is_valid():
+            friend_data_one.save()
+            friend_data_two.save()
+
+            return Response(data=[friend_data_one.data, friend_data_two.data], status=status.HTTP_201_CREATED)
+
+        return Response([friend_data_one.errors, friend_data_two.errors], status=status.HTTP_400_BAD_REQUEST)
