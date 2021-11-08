8954dd38fa431e4f0a4a4fcb992001d421693886
Shawn-NG <nghengdi10@gmail.com>
2021-11-08 17:19:34 +0000

    commenting on the configuration of POST method

--- Friend/views.py
+++ Friend/views.py
@@ -12,13 +12,14 @@ class FriendViewSet(viewsets.ModelViewSet):
     serializer_class = FriendSerializer
 
     def create(self, request):
-        # {'user_friend_id_one': 'cf48c865-2db7-4be4-9006-d5595fca829d', 'user_friend_id_two': '35367163-c482-401d-b621-4c90f62594f6'}
+        # creating two type of data ( two row ) this is to create a birectional graph
         friend_data_one = FriendSerializer(data=request.data)
         friend_data_two = FriendSerializer(data={
             'user_friend_id_one': request.data['user_friend_id_two'],
             'user_friend_id_two': request.data['user_friend_id_one']
         })
 
+        # saving the data and uploading it
         if friend_data_one.is_valid() and friend_data_two.is_valid():
             friend_data_one.save()
             friend_data_two.save()
