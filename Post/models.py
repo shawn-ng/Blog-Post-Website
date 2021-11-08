e4c171bb15c413a75b93309eff8ffe9c03fe36d5
Shawn-NG <nghengdi10@gmail.com>
2021-11-08 14:21:02 +0000

    resolving error

--- Post/models.py
+++ Post/models.py
@@ -12,5 +12,5 @@ class Post(models.Model):
     profle_id_post = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     image_url = models.URLField(blank=True, null=True)
     post_paragraph = models.TextField(blank=True, null=True)
-    post_description = models.TextField(max_length=500, blanl=True, null=True)
+    post_description = models.TextField(max_length=500, blank=True, null=True)
     post_created = models.DateTimeField(auto_now_add=True)
