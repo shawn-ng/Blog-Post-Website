c435b0bec8f3b59b05aef6748ec5ef069da71b90
Shawn-NG <nghengdi10@gmail.com>
2021-11-08 11:35:37 +0000

    changing the comment on on_delete

--- UserProfile/models.py
+++ UserProfile/models.py
@@ -8,7 +8,7 @@ from User.models import User
 class UserProfile(models.Model):
     profile_id = models.UUIDField(
         primary_key=True, editable=False, default=uuid.uuid4, unique=True)
-    # on_delete models.CASCADE is when i delete this profile the reference will also be deleted
+    # on_delete models.CASCADE is when i delete the user the reference/ user profile will also be deleted
     user_id_profile = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField(max_length=500, blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
