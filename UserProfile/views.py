fe2caca37d56504fe9d42ff32fe5592b51decd06
Shawn-NG <nghengdi10@gmail.com>
2021-11-11 18:45:09 +0000

    permission implemented into creating user profile

--- UserProfile/views.py
+++ UserProfile/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from .serializer import UserProfileSerializers
 from .models import UserProfile
@@ -11,3 +12,10 @@ from .models import UserProfile
 class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializers
+    permission_classes = [IsAuthenticated]
+
+    # this is to only get the login user profile
+    def get_queryset(self):
+        user = self.request.user
+        data = UserProfile.objects.filter(user_id_profile=user)
+        return data
